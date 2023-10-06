08714505d3990d9c6d3a054c773a67fd27b7d044
Juane <juanehueda885@gmail.com>
2023-10-06 15:48:10 -0300

    changes

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-userInput.addEventListener('submit', ()=>{
-    console.log('sea ha disparado el evento submit')
-});
+const input = document.querySelector('.input-text');
+const addform = document.querySelector('#userInput');
+const tasksList = document.querySelector('.list-container');
+const deleteBtn = document.querySelector('.closeBtn');
+
+let tasks = [];
+
+const addTask = (event) => {
+    event.preventDefault();
+    const taskName = input.value.trim();
+
+    if(!taskName.length) {
+        alert('Por favor, ingresa una tarea');
+        return;
+    }
+
+    tasks = [... tasks, {name: taskName, taskId: tasks.length + 1}];
+};
+
+
+const init = () => {
+    addform.addEventListener('submit', addTask )
+};
+
+init();
