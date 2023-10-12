27825d19463b7fe181da359964f6ddaf5268d172
Juane <juanehueda885@gmail.com>
2023-10-12 11:01:07 -0300

    funciones agregadas

--- main.js
+++ main.js
@@ -3,8 +3,26 @@ const addform = document.querySelector('#userInput');
 const tasksList = document.querySelector('.list-container');
 const deleteBtn = document.querySelector('.closeBtn');
 
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
+
+const saveLocalStorage = tasksList => {
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+};
+
+
+const createTask = task =>
+    `<li>${task.name}<img class='closeBtn' src='./assets/pngwing.com.png' data-id=${task.taskId} /</li>`
+
+
+
+// logica de renderizacion
+const renderTasksList = todoList => {
+    tasksList.innerHTML = todoList.map(task => createTask(task)).join('');
+};
+
+
+// funcion para agregar tareas
 const addTask = (event) => {
     event.preventDefault();
     const taskName = input.value.trim();
@@ -12,13 +30,26 @@ const addTask = (event) => {
     if(!taskName.length) {
         alert('Por favor, ingresa una tarea');
         return;
+    } else if(
+        tasks.some(task => task.name.toLowerCase() === taskName.toLowerCase())
+    ) {
+        alert ('Ya existe esa tarea');
+        return;
     }
 
+
+
+    
     tasks = [... tasks, {name: taskName, taskId: tasks.length + 1}];
+    input.value = '';
+
+    renderTasksList(tasks);
+    saveLocalStorage(tasks);
 };
 
 
 const init = () => {
+    renderTasksList(tasks);
     addform.addEventListener('submit', addTask )
 };
 
